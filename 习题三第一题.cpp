3a330140b639f8aec3730433c5022d325f7bf9e4
wangjianjin <32037590+wangjianjin@users.noreply.github.com>
2017-10-15 23:06:54 +0800

    Add files via upload

---
+++ 习题三第一题.cpp
@@ -0,0 +1,41 @@
+#include<iostream.h>
+void main()
+{
+	fioat x,y;
+	cout<<"please inputx:"<<endl;
+	cin>>x;
+	if(x<10)
+		y=A;
+	else
+		if(x>=10&&x<=99)
+			y=B;
+		else
+			if(x>=100&&x<=999)
+				y=C;
+			else
+				if(x>=1000)
+					y=D
+	cout<<y<<endl;
+}
+
+
+
+
+
+#include<iostream.h>
+void main()
+{
+	cout<<"please input x:";
+	cin>>x;
+	switch(x)
+	{
+		int x;
+		cout<<x<10<<":A\n";break;
+		cout<<x>=10&&x<=99<<":B\n";break;
+		cout<<x>=100&&x<=999<<":C\n";break;
+		cout<<x>=1000<<":D\n";break;
+	}
+}
+
+
+
