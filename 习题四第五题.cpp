19d832e55b6c9a5d397b294578ef19eb6c71c554
wangjianjin <32037590+wangjianjin@users.noreply.github.com>
2017-10-15 23:09:36 +0800

    Add files via upload

---
+++ 习题四第五题.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+void main()
+{
+	int x,y;
+	for(x=0;x<=9;x=x++)
+	{
+		for(y=0,y<=9;y=y++)
+	}
+	if((10*x+y)*(10*x+y)<10&&(10*x+y)*(10*x+y)=10*x+y);
+	{
+		cout<<10*x+y<<"\t"
+	}
+	lese
+	if((10*x+y)*(10*x+y)<10&&(10*x+y)*(10*x+y)>=10&&(10*x+y)*(10*x+y)%10=10*x+y)
+	{
+		cout<<10*x+y<<"\t";
+	}
+}
