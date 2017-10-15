47f3431fabc0e1ee603c6e08ebca0ec1cd80b0ca
wangjianjin <32037590+wangjianjin@users.noreply.github.com>
2017-10-15 23:06:24 +0800

    Add files via upload

---
+++ 习题三第三题.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+void main()
+{
+	int x,y;
+	cout<<"please input number:"<<endl;
+	cin>>x;
+	if(x%3==0&&x%7==0)
+		y=1;
+	else
+		y=0;
+		if(y==1)
+			cout<<"yes"£»
+			else
+			cout<<"no"
+}
