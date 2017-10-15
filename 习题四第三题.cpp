07cbcc797aad409e31d8507ed372b9e029292e10
wangjianjin <32037590+wangjianjin@users.noreply.github.com>
2017-10-15 23:09:20 +0800

    Add files via upload

---
+++ 习题四第三题.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+void main()
+{
+	int x,y,z,i;
+	y=0,z=0;
+	cout<<"输入一个正整数x"<<endl;
+	cin>>x;
+	while(x!=1)
+	{
+		if(x%2==0)
+	}
+	x=x/2;
+	y=y+1;
+	{
+		else
+	}
+	x=x*3+1;
+	z=z+i;
+	{
+		i=y+z;
+	}
+	cout<<"z="<<z<<endl;
+}
+
