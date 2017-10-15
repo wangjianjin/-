c760b4ab937059946d78c869b0ebbbc727b92d2f
wangjianjin <32037590+wangjianjin@users.noreply.github.com>
2017-10-15 23:07:52 +0800

    Add files via upload

---
+++ 习题三第六题.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<iomanip.h>
+void main()
+{
+	float r,l,s,pi;
+	int k;
+	pi=3.14159;
+	l=2*pi*r;
+	s=pi*r*r;
+	switch(k)
+	{
+	case 1:cout<<s;break;
+	case 2:cout<<l;break;
+	case 3:cout<<l<<s;break;
+	}
+}
